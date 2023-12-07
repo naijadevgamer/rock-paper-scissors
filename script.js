0c1ac7b86bd5f68e9e6fcf21a808af27ea6fd48e
thenaijagamer <sabdullahialaba50@gmail.com>
2023-12-07 14:35:41 +0100

    Add the select classic icon functionality

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+const classic = document.querySelectorAll(".phase--classic .icon");
+console.log(classic);
+
+for (let i = 0; i < classic.length; i++) {
+  const e = classic[i];
+  // console.log(e.classList.contains("icon--rock"));
+  e.addEventListener("click", (e) => {
+    // e.stopPropagation();
+    console.log(e.currentTarget);
+  });
+}
+
+// const playerPick = () => {};
