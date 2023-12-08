559218cba005dc54d6ae1897121d54ba16276659
thenaijagamer <sabdullahialaba50@gmail.com>
2023-12-08 11:03:43 +0100

    Add the show picked functionality

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 "use strict";
 const classic = document.querySelectorAll(".phase--classic .icon");
-console.log(classic);
 
 const rps = ["rock", "paper", "scissors"];
+
 for (let i = 0; i < classic.length; i++) {
-  // console.log(e.classList.contains("icon--rock"));
-  e.addEventListener("click", (e) => {
-    // e.stopPropagation();
-    console.log(e.currentTarget.classList.contains(`icon--rock`));
+  classic[i].addEventListener("click", (e) => {
+    const present = e.currentTarget.classList.contains(`icon--${rps[i]}`);
+    const randNum = Math.floor(Math.random() * 3);
+    console.log(randNum);
+    if (present) {
+      // Hide the choosing phase and show the next step
+      document.querySelector(".phase--classic").classList.add("hidden");
+      document.querySelector(".phase__comparism").classList.remove("hidden");
+
+      // show what the player picked
+      document
+        .querySelector(".phase__pick--player .icon")
+        .classList.add(`icon--${rps[i]}`);
+      document.querySelector(
+        ".phase__pick--player .icon__img"
+      ).src = `images/icon-${rps[i]}.svg`;
+
+      setTimeout(() => {
+        // show what the house picked
+        document
+          .querySelector(".phase__pick--house .icon")
+          .classList.remove("hidden");
+        document
+          .querySelector(".phase__pick--house .icon")
+          .classList.add(`icon--${rps[randNum]}`);
+        document.querySelector(
+          ".phase__pick--house .icon__img"
+        ).src = `images/icon-${rps[randNum]}.svg`;
+        document.querySelector(
+          ".phase__pick--house .icon__under"
+        ).style.position = "absolute";
+      }, 1000);
+    }
   });
 }
 
